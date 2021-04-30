972bc2c23f94e00cfecb1956d838e190284d50fd
FE1979 <ronin911@gmail.com>
2021-04-30 21:23:22 +0300

    flow.py and models.py returned to the app

---
+++ boards/baraxolka/flow.py
@@ -0,0 +1,18 @@
+"""
+Main script that runs data logic
+"""
+#imports
+
+#general settings
+
+#scrap threads and save them to the file
+
+#compare to the existing in database
+#choose only:
+#   1) new threads;
+#   2) existed in db but with update;
+
+#load chosen threads and save data in db
+
+#check 'closed' threads and move them into archive.
+#check old threads and move them into archive
