9932c9c911d3796a3d8430a42dbd2478c78e6345
FE1979 <ronin911@gmail.com>
2021-04-30 21:22:31 +0300

    restore baraxolka app

--- boards/baraxolka/models.py
+++ boards/baraxolka/models.py
@@ -1,28 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Thread(models.Model):
-    """ Main table with a list of threads """
-
-    pinned = models.BooleanField()
-    thread_id = models.PositiveInteger(primary_key=True)
-    author_name = models.CharField(max_length=256)
-    author_link = models.URLField()
-    title = models.CharField(max_length=256)
-    Update_date = models.DateField(auto_now=False)
-    url = models.URLField()
-
-
-class Post(models.Model):
-    """ Text information in the thread """
-
-    thread = models.ForeignKey(Thread)
-    text = models.TextField()
-    post_date = models.DateField(auto_now=False)
-
-
-class Attachment(models.Model):
-    """ references to any attachments in the thread """
-    
-    thread = models.ForeignKey(Thread)
-    url = URLField()
